532bf67fb9a5c0df0e84060d843e41abddc17788
Pallab <pallabkshyp@gmail.com>
2024-11-02 02:14:20 +0530

    meta

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,24 @@
-
-import './App.css'
-import Home from './pages/Home'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import "./App.css";
+import Home from "./pages/Home";
+import { Helmet } from "react-helmet";
 
 function App() {
-
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<Home />} />
-      </Routes>
-    </Router>
-  )
+    <>
+      <Helmet>
+        <title>Consultify - Your Tech Partners</title>
+        <meta
+           name="description"
+           content="Specializing in web development, design, SEO, and social media marketing, we empower businesses with digital strategies that boost brand visibility and drive growth."
+         />
+         <meta
+           name="keywords"
+           content="web development services, website design agency, SEO optimization, social media marketing, digital marketing strategy, user experience design, custom website solutions"
+         />
+      </Helmet>
+      <Home />
+    </>
+  );
 }
 
-export default App
+export default App;
